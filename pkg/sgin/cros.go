ba65453bc9e693195978292292f8a23b275d1ca7
wsd <359068189@qq.com>
2025-05-15 12:09:23 +0800

    -

---
+++ pkg/sgin/cros.go
@@ -0,0 +1,28 @@
+package sgin
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "*")
+		c.Header("Access-Control-Max-Age", "86400")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func (s *Srv) WithCors() *Srv {
+	s.Use(cors())
+	return s
+}
